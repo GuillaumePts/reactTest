f76fa032cbc3d963e321893fceaf5159a519542d
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-07 15:34:15 +0200

    création du header

---
+++ src/header/header.js
@@ -0,0 +1,20 @@
+import './header.css'
+
+function Header(){
+    return (
+        
+
+            <header>
+            <h1>mon logo</h1>
+            <ul>
+                <li><a href='#'>menu1</a></li>
+                <li><a href='#'>menu2</a></li>
+                <li><a href='#'>menu3</a></li>
+                <li><a href='#'>menu4</a></li>
+            </ul>
+            </header>
+        
+    );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Footer from '../src/footer/footer';
+import Header from '../src/header/header';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header/>
+    
     <App />
+
     <Footer/>
+    
   </React.StrictMode>
 );
