5a147f3e2b49e65f0d5ce976924d849864988bc5
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-07 15:00:06 +0200

    création du footer

---
+++ src/footer/footer.js
@@ -0,0 +1,16 @@
+import './footer.css'
+
+
+function Footer(){
+    return (
+<div className="footer">
+    <footer>
+        <p>
+        &copy; powered by moi 
+        </p>
+    </footer>
+</div>
+    );
+}
+
+export default Footer;
