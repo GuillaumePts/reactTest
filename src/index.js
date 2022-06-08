4622c4710b3568a564306400daa155ba53712406
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-06-08 11:41:56 +0200

    decouverte tableau avec key et utilisation de .map

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Footer from '../src/footer/footer';
 import Header from '../src/header/header';
+import Emoji from '../src/Emoji/Emoji';
 
 
 
@@ -11,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
-    
+    <Emoji/>
     <App />
 
     <Footer/>
